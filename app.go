8c9606847573e75a40e83e2d0c3f552021ffbe91
Jeff Halmich <jeff91jr@gmail.com>
2015-03-28 12:07:12 -0400

    dokku

--- app.go
+++ app.go
@@ -1,32 +1,34 @@
 package main
 
 import (
-	"encoding/base64"
-	"errors"
+	// "encoding/base64"
+	// "errors"
+	// "io/ioutil"
+	// "strings"
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/neoncode/NoSQLDataAccess"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	"strings"
+	 "log"
+	 "net/http"
+	 "os"
 )
-
+var url string
 func main() {
-	// Set up configuration
-	url := os.Getenv("DBPATH")
-	if url == "" {
-		url = "http://localhost:8091/"
-	}
+	//Set up configuration
+	url = "woo"
+	// url = os.Getenv("DBPATH")
+	// if url == "" {
+	// 	url = "http://localhost:8091/"
+	// }
 
-	_ = url
+	// _ = url
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/SecretThing/{key}", func(w http.ResponseWriter, r *http.Request) {
-		DecorateWithLog(SecretThingEndpoint)(w, r)
-	})
+	// router.HandleFunc("/SecretThing/{key}", func(w http.ResponseWriter, r *http.Request) {
+	// 	DecorateWithLog(SecretThingEndpoint)(w, r)
+	// })
+	//thing = SecretThing
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
-
+	fmt.Println(os.Getenv("PORT"))
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 }
 
@@ -47,77 +49,80 @@ func SecretThingEndpoint(w http.ResponseWriter, r *http.Request) error {
 }
 
 func GetAuthenticationString(w http.ResponseWriter, r *http.Request) ([]byte, error) {
-	auth := strings.SplitN(r.Header["Authorization"][0], " ", 2)
-	fmt.Println("made it past getting the header")
-	if len(auth) != 2 || auth[0] != "Basic" {
-		http.Error(w, "bad syntax", http.StatusBadRequest) //This is a good strategy for handling errors
-		return nil, errors.New("Bad Syntax")
-	}
+	// auth := strings.SplitN(r.Header["Authorization"][0], " ", 2)
+	// fmt.Println("made it past getting the header")
+	// if len(auth) != 2 || auth[0] != "Basic" {
+	// 	http.Error(w, "bad syntax", http.StatusBadRequest) //This is a good strategy for handling errors
+	// 	return nil, errors.New("Bad Syntax")
+	// }
 
-	payload, err := base64.StdEncoding.DecodeString(auth[1])
-	return payload, err
+	// payload, err := base64.StdEncoding.DecodeString(auth[1])
+	// return payload, err
+	return nil,nil
 }
 
-func GetSecretThingFromRequest(r *http.Request) (*SecretThing, error) {
-	vars := mux.Vars(r)
-	key := vars["key"]
-	bytes, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return nil, err
-	}
-	secretThing := SecretThing{key, bytes}
-	return &secretThing, err
-}
+// func GetSecretThingFromRequest(r *http.Request) (*SecretThing, error) {
+// 	// vars := mux.Vars(r)
+// 	// key := vars["key"]
+// 	// bytes, err := ioutil.ReadAll(r.Body)
+// 	// if err != nil {
+// 	// 	return nil, err
+// 	// }
+// 	// secretThing := SecretThing{key, bytes}
+// 	// return &secretThing, err
+// 	return nil, nil
+// }
 
 func PutOrPostSecretThing(w http.ResponseWriter, r *http.Request) (err error) {
-	thing, err := GetSecretThingFromRequest(r)
-	if err != nil {
-		return
-	}
+	// thing, err := GetSecretThingFromRequest(r)
+	// if err != nil {
+	// 	return
+	// }
 
-	url := "http://localhost:8091/"
+	// url := "http://localhost:8091/"
 
-	authString, err := GetAuthenticationString(w, r)
-	if err != nil {
-		return
-	}
+	// authString, err := GetAuthenticationString(w, r)
+	// if err != nil {
+	// 	return
+	// }
 
-	encrypted, err := Encrypt(Hash(authString), thing.Value)
-	if err != nil {
-		return
-	}
+	// encrypted, err := Encrypt(Hash(authString), thing.Value)
+	// if err != nil {
+	// 	return
+	// }
 
-	thing.Value = encrypted
+	// thing.Value = encrypted
 
-	couchbase := DataAccess.GetCouchbaseDAL(url, "default", "SecretThing")
+	// couchbase := DataAccess.GetCouchbaseDAL(url, "default", "SecretThing")
 
-	err = couchbase.Set(thing.Key, thing)
+	// err = couchbase.Set(thing.Key, thing)
 	return
 }
 
 func GetSecretThing(w http.ResponseWriter, r *http.Request) (err error) {
-	url := "http://localhost:8091/"
-	thing := new(SecretThing)
-	vars := mux.Vars(r)
-	key := vars["key"]
-	couchbase := DataAccess.GetCouchbaseDAL(url, "default", "SecretThing")
-	err = couchbase.Get(key, thing)
-	fmt.Println(err)
-	if err != nil {
-		return
-	}
+	// url := "http://localhost:8091/"
+	// thing := new(SecretThing)
+	// vars := mux.Vars(r)
+	// key := vars["key"]
+	// couchbase := DataAccess.GetCouchbaseDAL(url, "default", "SecretThing")
+	// err = couchbase.Get(key, thing)
+	// fmt.Println(err)
+	// if err != nil {
+	// 	return
+	// }
 
-	authString, err := GetAuthenticationString(w, r)
-	if err != nil {
-		return
-	}
+	// authString, err := GetAuthenticationString(w, r)
+	// if err != nil {
+	// 	return
+	// }
 
-	decrypted, err := Decrypt(Hash(authString), thing.Value)
-	if err != nil {
-		return
-	}
+	// decrypted, err := Decrypt(Hash(authString), thing.Value)
+	// if err != nil {
+	// 	return
+	// }
 
-	w.Write(decrypted)
+	// w.Write(decrypted)
+	// return
 	return
 }
 
@@ -139,3 +144,32 @@ func DecorateWithLog(fn appHandler) func(http.ResponseWriter, *http.Request) {
 		}
 	}
 }
+
+
+// package main
+// import (
+//   // "fmt"
+//   "net/http"
+//   "github.com/gorilla/mux"
+//   "os"
+//   "log"
+// )
+
+// // func handler ...
+
+// func main() {
+// 	router := mux.NewRouter().StrictSlash(true)
+
+// 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
+
+// 	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), router))
+// }
+
+// func handler(w http.ResponseWriter, r *http.Request) {
+//   fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+// }
+
+// func main() {
+//   http.HandleFunc("/", handler)
+//   http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+// }
